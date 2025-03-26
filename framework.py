3479c71a46e82ac9c63fe3490e6b9021b4def5c6
Alexandre Girard <alx87grd@gmail.com>
2025-03-26 09:48:04 -0400

    Added source block

--- framework.py
+++ framework.py
@@ -1,5 +1,4 @@
 import numpy as np
-import graphviz
 
 
 ######################################################################
@@ -13,12 +12,12 @@ class Port:
         self.units = [""] * self.n
         self.upper_bound = np.inf * np.ones(self.n)
         self.lower_bound = -np.inf * np.ones(self.n)
-        
+
 
 ######################################################################
 class InputPort(Port):
 
-    def get(self, t=0) -> np.ndarray:
+    def get_signal(self, t=0) -> np.ndarray:
         return self.default_value
 
 
@@ -47,17 +46,6 @@ class System:
         self.p = p
 
         self.name = name
-        self.latex = '$\dot{x} = f(x, u, t)$\n$y = h(x, u, t)$'
-
-        self.inputs = {
-            "u": InputPort("u", self.m),
-            # "r": InputPort("r", self.m),
-            }
-        self.outputs = {
-            "y": OutputPort("y", self.p, function=self.h),
-            # "x": OutputPort("x", self.n, function=self.hx),
-            # "q": OutputPort("q", self.n, function=self.hq),
-        }
 
     ######################################################################
     def f(self, x, u, t=0, params=None) -> np.ndarray:
@@ -72,7 +60,7 @@ class System:
     ######################################################################
     def compute_state(self, x, u, t=0, params=None):
         return x
-    
+
     ######################################################################
     def fsim(self, x, t=0):
         inputs = self.collect_inputs(t)
@@ -88,8 +76,10 @@ class System:
     ######################################################################
     def compute_input_dimensions(self):
         self.m = 0
-        for (key, port) in self.inputs.items():
+        for key, port in self.inputs.items():
             self.m += port.n
+
+        return self.m
 
     ######################################################################
     def fsim(self, x, t=0):
@@ -99,26 +89,24 @@ class System:
 
     ######################################################################
     def collect_inputs(self):
-        input_dict = {}
-        for (key, port) in self.inputs.items():
-            input_dict[key] = port.get()
-        return input_dict
-    
+        input_signals = {}
+        for key, port in self.inputs.items():
+            input_signals[key] = port.get_signal()
+        return input_signals
+
     ######################################################################
-    def inputs2u(self, input_dict):
-        return np.concatenate(list(input_dict.values()))
+    def inputs2u(self, input_signals):
+        return np.concatenate(list(input_signals.values()))
 
     ######################################################################
     def u2inputs(self, u):
-        input_dict = {}
+        input_signals = {}
         idx = 0
         for key, port in self.inputs.items():
-            input_dict[key] = u[idx:idx + port.n]
+            input_signals[key] = u[idx : idx + port.n]
             idx += port.n
-        return input_dict
-    
+        return input_signals
 
-    
     ######################################################################
     def print_html(self):
 
@@ -130,41 +118,44 @@ class System:
 
         display.display(display.HTML(self.get_block_html()))
 
-
     ######################################################################
-    def get_block_html(self, label='sys1'):
+    def get_block_html(self, label="sys1"):
 
         n_ports_out = len(self.outputs)
         n_ports_in = len(self.inputs)
 
         label = (
-            F'<TABLE BORDER="0" CELLSPACING="0">\n'
-            F'<TR>\n'
-            F'<TD align="left" BORDER="1" COLSPAN="2">{self.name}::{label}</TD>\n'
-            F'</TR>\n'
+            f'<TABLE BORDER="0" CELLSPACING="0">\n'
+            f"<TR>\n"
+            f'<TD align="left" BORDER="1" COLSPAN="2">{self.name}::{label}</TD>\n'
+            f"</TR>\n"
         )
 
         for j in range(np.max((n_ports_out, n_ports_in))):
-            label += F'<TR>\n'
+            label += f"<TR>\n"
 
             if j < n_ports_in and j < n_ports_out:
                 port_id = list(self.inputs.keys())[j]
-                label += F'<TD PORT="{port_id}" align="left" BORDER="1">{port_id}</TD>\n'
+                label += (
+                    f'<TD PORT="{port_id}" align="left" BORDER="1">{port_id}</TD>\n'
+                )
                 port_id = list(self.outputs.keys())[j]
-                label += F'<TD PORT="{port_id}" BORDER="1">{port_id}</TD>\n'
+                label += f'<TD PORT="{port_id}" BORDER="1">{port_id}</TD>\n'
 
             elif j < n_ports_in:
                 port_id = list(self.inputs.keys())[j]
-                label += F'<TD PORT="{port_id}" align="left" BORDER="1">{port_id}</TD>\n'
-                label += F'<TD BORDER="1"> </TD>\n'
+                label += (
+                    f'<TD PORT="{port_id}" align="left" BORDER="1">{port_id}</TD>\n'
+                )
+                label += f'<TD BORDER="1"> </TD>\n'
 
             elif j < n_ports_out:
                 port_id = list(self.outputs.keys())[j]
-                label += F'<TD BORDER="1"> </TD>\n'
-                label += F'<TD PORT="{port_id}" BORDER="1">{port_id}</TD>\n'
-            
-            label += F'</TR>\n'
-        label += F'</TABLE>'
+                label += f'<TD BORDER="1"> </TD>\n'
+                label += f'<TD PORT="{port_id}" BORDER="1">{port_id}</TD>\n'
+
+            label += f"</TR>\n"
+        label += f"</TABLE>"
 
         return label
 
@@ -177,8 +168,75 @@ class StaticSystem(System):
         System.__init__(self, 0, m, p)
         self.name = "StaticSystem"
 
-    def f(self, x, u, t):
+        self.inputs = {
+            "u": InputPort("u", self.m),
+            # "r": InputPort("r", self.m),
+        }
+        self.outputs = {
+            "y": OutputPort("y", self.p, function=self.h),
+        }
+
+    def f(self, x, u, t=0, params=None):
         raise Exception("Static system has no state")
+
+
+######################################################################
+class Source(System):
+
+    def __init__(self, p):
+
+        System.__init__(self, 0, 0, p)
+
+        self.name = "Source"
+        params = {"value": np.zeros(p)}
+
+        self.inputs = {}
+        self.outputs = {
+            "y": OutputPort("y", self.p, function=self.h),
+        }
+
+    def f(self, x, u, t=0, params=None):
+        raise Exception("Source system has no state")
+
+    def h(self, x, u, t=0, params=None):
+
+        #
+        if params is None:
+            params = self.params
+
+        y = params["value"]
+        return y
+
+
+######################################################################
+class Step(Source):
+
+    def __init__(
+        self, intial_value=np.zeros(1), final_value=np.zeros(1), step_time=1.0
+    ):
+
+        p = intial_value.shape[0]
+        Source.__init__(self, p)
+
+        self.name = "Step"
+        params = {
+            "initial_value": intial_value,
+            "final_value": final_value,
+            "step_time": step_time,
+        }
+
+    def h(self, x, u, t=0, params=None):
+
+        #
+        if params is None:
+            params = self.params
+
+        if t < params["step_time"]:
+            y = params["initial_value"]
+        else:
+            y = params["final_value"]
+
+        return y
 
 
 ######################################################################
@@ -191,6 +249,16 @@ class DynamicSystem(System):
 
         System.__init__(self, n, m, p, name, params)
 
+        self.inputs = {
+            "u": InputPort("u", self.m),
+            # "r": InputPort("r", self.m),
+        }
+
+        self.outputs = {
+            "y": OutputPort("y", self.p, function=self.h),
+            "x": OutputPort("x", self.n, function=self.compute_state),
+        }
+
 
 ######################################################################
 class GrapheSystem(System):
@@ -202,12 +270,12 @@ class GrapheSystem(System):
 
         System.__init__(self, 0, 0, 0)
 
-        self.name = 'Diagram'
+        self.name = "Diagram"
 
         self.inputs = {
             "u": InputPort("u", self.m),
             # "r": InputPort("r", self.m),
-            }
+        }
         self.outputs = {
             "y": OutputPort("y", self.p, function=self.h),
             # "x": OutputPort("x", self.n, function=self.hx),
@@ -248,6 +316,13 @@ class GrapheSystem(System):
         )
 
     def render_graphe(self):
+
+        try:
+            import graphviz
+        except ImportError:
+            print("graphviz is not available")
+            return None
+
         g = graphviz.Digraph("G", filename="testgraphe.gv", engine="dot")
         g.attr(rankdir="LR")
         g.attr(concentrate="true")
@@ -256,7 +331,7 @@ class GrapheSystem(System):
 
             print(str(i))
 
-            label = F'<{sys.get_block_html(sys_id)}>'
+            label = f"<{sys.get_block_html(sys_id)}>"
 
             g.node(
                 sys_id,
@@ -285,6 +360,7 @@ if __name__ == "__main__":
     sys2 = DynamicSystem(2, 1, 1)
     sys3 = StaticSystem(1, 1)
     sys4 = DynamicSystem(2, 1, 1)
+    step = Step(np.array([0.0]), np.array([1.0]), 1.0)
 
     sys1.print_html()
     sys1.add_input_port("w", 2)
@@ -292,31 +368,31 @@ if __name__ == "__main__":
     sys1.add_input_port("v", 1)
     sys1.print_html()
 
-
     sys1.inputs["u"].default_value = np.array([7.7])
     sys1.inputs["w"].default_value = np.array([1.1, 2.2])
 
     inputs = sys1.collect_inputs()
-    u = sys1.inputs2u( inputs )
+    u = sys1.inputs2u(inputs)
     print(inputs)
     print(u)
     print(sys1.u2inputs(u))
 
-    # gsys = GrapheSystem()
-    # gsys.print_html()
-    # gsys.add_system(sys1, "sys1")
-    # gsys.add_system(sys2, "sys2")
-    # gsys.add_system(sys3, "sys3")
-    # gsys.add_system(sys4, "sys4")
-
-    # # gsys.render_graphe()
-
-    # gsys.add_edge("sys1", "y", "sys2", "u")
-    # gsys.add_edge("sys2", "y", "sys3", "u")
-    # gsys.add_edge("sys2", "y", "sys4", "u")
-    # gsys.add_edge("sys4", "y", "sys1", "u")
-
+    gsys = GrapheSystem()
+    gsys.print_html()
+    gsys.add_system(sys1, "sys1")
+    gsys.add_system(sys2, "sys2")
+    gsys.add_system(sys3, "sys3")
+    gsys.add_system(sys4, "sys4")
+    gsys.add_system(step, "step")
 
     # gsys.render_graphe()
+
+    gsys.add_edge("sys1", "y", "sys2", "u")
+    gsys.add_edge("sys2", "y", "sys3", "u")
+    gsys.add_edge("sys2", "y", "sys4", "u")
+    gsys.add_edge("sys4", "y", "sys1", "u")
+    gsys.add_edge("step", "y", "sys1", "v")
+
+    gsys.render_graphe()
 
     # print("Done.")
